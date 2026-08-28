e9d36f695b2c57521d6b246c4bf56b8467390afc
Muhammad Abdullah <abaloch0033@gmail.com>
2026-08-28 10:39:42 +0500

    Reorder className attributes in Section and Container

--- apps/web/src/app/services/[slug]/page.tsx
+++ apps/web/src/app/services/[slug]/page.tsx
@@ -49,8 +49,8 @@ export default async function ServiceDetailPage({ params }: { params: Promise<{ 
     <>
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
 
-      <Section className="pb-0 max-w-8xl">
-        <Container>
+      <Section className="pb-0">
+        <Container className="max-w-8xl">
           <PageHeading
             breadcrumb={[{ label: "Home", href: "/" }, { label: "Services", href: "/services" }, { label: service.name }]}
           >
